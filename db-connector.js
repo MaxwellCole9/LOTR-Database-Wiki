53a55508900b3970db8a25e51888bb7ffaff5fd6
Max Cole <colemax@oregonstate.edu>
2024-02-22 13:14:48 -0800

    added search functionality, further adjusted styles.

--- db-connector.js
+++ db-connector.js
@@ -1,3 +1,10 @@
+/*
+    Code utilized from the CS340 course at Oregon State University
+    - Utilized canvas suggestion to create a db-connector.js file to connect to the database
+    https://canvas.oregonstate.edu/courses/1946034/pages/exploration-developing-in-node-dot-js?module_item_id=23809336
+    2/22/2024
+*/
+
 var mysql = require('mysql')
 
 // Create a 'connection pool' using the provided credentials
